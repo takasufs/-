831802f767ef45f46208eda69c7f06a579cb0fcb
suzu <92066345+suzu-moka@users.noreply.github.com>
2022-09-26 10:06:46 +0900

    Squashed commit of the following:
    commit 03c678d6e472a9eb8d85cc3666f4bb6cdc55e139
    Author: takasufs <takato.suzuki2021@gmail.com>
    Date:   Thu Sep 22 16:57:12 2022 +0900
    
        a
    
    commit 2c02ef1737d13284b184c7c93cda065bd477fb75
    Author: takasufs <takato.suzuki2021@gmail.com>
    Date:   Thu Sep 22 16:50:01 2022 +0900
    
        tesseract js

---
+++ js/Tesseract.js
@@ -0,0 +1,50 @@
+//recognize関数の宣言
+const recognize = function (evt) {
+    //読み込むファイルの宣言
+    const files = evt.target.files;
+    //ファイルの中身がないときには実行しない
+    if (files.length == 0)
+    {
+        return;
+    }
+    //Tesseract.jsの実行
+    Tesseract
+        //OCR機能を実行する際の読み込むファイルや言語を設定（言語はlang: ''の中身をengやjpnに変更することで英語や日本語にすることが可能です）
+        .recognize(files[0], { lang: 'eng', tessedit_pageseg_mode: "RAW_LINE" })
+        .then(function (result) {
+            // 結果の表示
+            //テキストエリアを指定
+            let textarea = document.getElementById("ocrResult");
+            //テキストエリアに結果を埋め込む
+            textarea.value = result.text;
+        });
+}
+//関数宣言終了
+
+const recognize02 = function (evt) {
+    //読み込むファイルの宣言
+    const files = evt.target.files;
+    //ファイルの中身がないときには実行しない
+    if (files.length == 0)
+    {
+        return;
+    }
+    //Tesseract.jsの実行
+    Tesseract
+        //OCR機能を実行する際の読み込むファイルや言語を設定（言語はlang: ''の中身をengやjpnに変更することで英語や日本語にすることが可能です）
+        .recognize(files[1], { lang: 'eng', tessedit_pageseg_mode: "RAW_LINE" })
+        .then(function (result) {
+            // 結果の表示
+            //テキストエリアを指定
+            let textarea = document.getElementById("ocrResult02");
+            //テキストエリアに結果を埋め込む
+            textarea.value = result.text;
+        });
+}
+
+//関数を実行
+//ファイル選択ボタンを指定して宣言
+const elm = document.getElementById('uploader');
+//ファイルの中に写真が追加されたらrecognize関数を実行
+elm.addEventListener('change', recognize);
+elm.addEventListener('change', recognize02);

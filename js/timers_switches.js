e27278244176807e2154609704c1f18c03225769
takasufs <takato.suzuki2021@gmail.com>
2022-09-29 14:34:55 +0900

    タイマー追加 css 少しおかしいから削除から再生成お願い

--- js/timers_switches.js
+++ js/timers_switches.js
@@ -1,104 +1,87 @@
-(function(){
-    'use strict';
+{
+    const timer = document.getElementById('timer');
+    const start = document.getElementById('start');
+    const stop = document.getElementById('stop');
+    const reset = document.getElementById('reset');
 
-    //htmlのidからデータを取得
-    //取得したデータを変数に代入
+    let startTime;       // Startボタンクリック時の時刻
+    let timeoutid;       // ID
+    let elapsedTime = 0; // StartからStopまでの経過時間
 
-    var timer = document.getElementById('timer');
-    var start = document.getElementById('start');
-    var stop = document.getElementById('stop');
-    var reset = document.getElementById('reset');
+    function countUp() {
+        const d = new Date(Date.now() - startTime + elapsedTime);
+        /* padStart()で二桁または三桁固定表示とする */
+        const m = String(d.getMinutes()).padStart(2, '0');
+        const s = String(d.getSeconds()).padStart(2, '0');
+        /* 描画 */
+        timer.textContent = `${m}:${s}`;
 
-    //クリック時の時間を保持するための変数定義
-    var startTime;
-
-    //経過時刻を更新するための変数。 初めはだから0で初期化
-    var elapsedTime = 0;
-
-    //タイマーを止めるにはclearTimeoutを使う必要があり、そのためにはclearTimeoutの引数に渡すためのタイマーのidが必要
-    var timerId;
-
-    //タイマーをストップ -> 再開させたら0になってしまうのを避けるための変数。
-    var timeToadd = 0;
-
-
-    //ミリ秒の表示ではなく、分とか秒に直すための関数, 他のところからも呼び出すので別関数として作る
-    //計算方法として135200ミリ秒経過したとしてそれを分とか秒に直すと -> 02:15:200
-    function updateTimetText(){
-
-        //m(分) = 135200 / 60000ミリ秒で割った数の商　-> 2分
-        var m = Math.floor(elapsedTime / 60000);
-
-        //s(秒) = 135200 % 60000ミリ秒で / 1000 (ミリ秒なので1000で割ってやる) -> 15秒
-        var s = Math.floor(elapsedTime % 60000 / 1000);
-
-        //ms(ミリ秒) = 135200ミリ秒を % 1000ミリ秒で割った数の余り
-        // var ms = elapsedTime % 1000;
-
-
-        //HTML 上で表示の際の桁数を固定する　例）3 => 03　、 12 -> 012
-        //javascriptでは文字列数列を連結すると文字列になる
-        //文字列の末尾2桁を表示したいのでsliceで負の値(-2)引数で渡してやる。
-        m = ('0' + m).slice(-2); 
-        s = ('0' + s).slice(-2);
-        // ms = ('0' + ms).slice(-3);
-
-        //HTMLのid　timer部分に表示させる　
-        timer.textContent = m + ':' + s ;//+ ':' + ms;
-    }
-
-
-    //再帰的に使える用の関数
-    function countUp(){
-
-        //timerId変数はsetTimeoutの返り値になるので代入する
-        timerId = setTimeout(function(){
-
-            //経過時刻は現在時刻をミリ秒で示すDate.now()からstartを押した時の時刻(startTime)を引く
-            elapsedTime = Date.now() - startTime + timeToadd;
-            updateTimetText()
-
-            //countUp関数自身を呼ぶことで10ミリ秒毎に以下の計算を始める
+        timeoutid = setTimeout(() => {
+            //再帰呼び出し
             countUp();
-
-        //1秒以下の時間を表示するために10ミリ秒後に始めるよう宣言
-        },10);
+        }, 10);
     }
 
-    //startボタンにクリック時のイベントを追加(タイマースタートイベント)
-    start.addEventListener('click',function(){
+    // 状態:初期 または Reset直後
+    function setButtonStateInitial() {
+        start.classList.remove('inactive'); // 活性
+        stop.classList.add('inactive');    // 非活性
+        reset.classList.add('inactive');   // 非活性
+    }
 
-        //在時刻を示すDate.nowを代入
+    // 状態:タイマー動作中
+    function setButtonStateRunning() {
+        start.classList.add('inactive');   // 非活性
+        stop.classList.remove('inactive');  // 活性
+        reset.classList.add('inactive');  // 非活性
+    }
+
+    // 状態:タイマー停止中
+    function setButtonStateStopped() {
+        start.classList.add('inactive'); // 活性
+        stop.classList.add('inactive');    // 非活性
+        reset.classList.remove('inactive'); // 活性
+    }
+
+    // ボタンを'初期'状態とする
+    setButtonStateInitial()
+
+    // Startボタンクリック
+    // …タイマーを開始します
+    start.addEventListener('click', () => {
+        if (start.classList.contains('inactive') === true)
+        {
+            return;
+        }
+        // ボタンをタイマー'動作中'状態とする
+        setButtonStateRunning();
         startTime = Date.now();
-
-        //再帰的に使えるように関数を作る
         countUp();
     });
 
-    //stopボタンにクリック時のイベントを追加(タイマーストップイベント)
-    stop.addEventListener('click',function(){
-
-        //タイマーを止めるにはclearTimeoutを使う必要があり、そのためにはclearTimeoutの引数に渡すためのタイマーのidが必要
-       clearTimeout(timerId);
-
-
-        //タイマーに表示される時間elapsedTimeが現在時刻かたスタートボタンを押した時刻を引いたものなので、
-        //タイマーを再開させたら0になってしまう。elapsedTime = Date.now - startTime
-        //それを回避するためには過去のスタート時間からストップ時間までの経過時間を足してあげなければならない。elapsedTime = Date.now - startTime + timeToadd (timeToadd = ストップを押した時刻(Date.now)から直近のスタート時刻(startTime)を引く)
-       timeToadd += Date.now() - startTime;
+    // Stopボタンクリック
+    // …タイマーを停止します
+    stop.addEventListener('click', () => {
+        if (stop.classList.contains('inactive') === true)
+        {
+            return;
+        }
+        // タイマーを'停止中'状態とする
+        setButtonStateStopped();
+        clearTimeout(timeoutid);
+        elapsedTime += Date.now() - startTime;
     });
 
-    //resetボタンにクリック時のイベントを追加(タイマーリセットイベント)
-    reset.addEventListener('click',function(){
-
-        //経過時刻を更新するための変数elapsedTimeを0にしてあげつつ、updateTimetTextで0になったタイムを表示。
+    // Resetボタンクリック
+    // …タイマーを「00:00.000」で上書きします
+    reset.addEventListener('click', () => {
+        if (reset.classList.contains('inactive') === true)
+        {
+            return;
+        }
+        // ボタンを'初期'状態とする
+        setButtonStateInitial()
+        timer.textContent = '00:00';
         elapsedTime = 0;
-
-        //リセット時に0に初期化したいのでリセットを押した際に0を代入してあげる
-        timeToadd = 0;
-
-        //updateTimetTextで0になったタイムを表示
-        updateTimetText();
-
     });
-})();
+}
